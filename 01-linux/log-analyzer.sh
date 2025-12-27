c44f884a1eebfd114972d0a244ab5862e6e2dcf9
taruneshwar28 <taruneshwar28@gmail.com>
2025-12-27 20:24:58 +0530

    Complete 01-linux exercise: log analyzer

---
+++ 01-linux/log-analyzer.sh
@@ -0,0 +1,22 @@
+LOG_FILE="/c/Users/Tarun/Desktop/revature/sample-log.txt"
+echo "===================LOG ANALYSIS REPORT================="
+echo "File: $LOG_FILE"
+if [ ! -e $LOG_FILE ]; then
+        echo "Error : Log file not found";
+else
+        count=0
+        while read line; do
+                ((count++))
+        done < sample-log.txt
+        echo "Total Lines: $count"
+        echo "-------------------------------------------------"
+        echo "INFO: $(grep -c -i 'INFO' "$LOG_FILE")"
+        echo "WARNING: $(grep -c -i 'WARNING' "$LOG_FILE")"
+        echo "ERROR: $(grep -c -i 'error' "$LOG_FILE")"
+        echo "-----------------------------------------------"
+        echo "Unique IP Addresses Found:"
+        IP_LIST=$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' "$LOG_FILE" | sort | uniq)
+        echo $IP_LIST
+        echo "==============================================="
+fi
+
