579a7e3672c4003168852497d6a13a9ed8700fac
Narava Tarun Eshwar <142734331+taruneshwar28@users.noreply.github.com>
2025-12-28 20:42:11 +0530

    Complete 06-javascript exercise: basic-quiz

---
+++ 06-javascript/basic-quiz.js
@@ -0,0 +1,14 @@
+let count=0;
+let q1=prompt("What is 5+3?")
+if(q1==8){
+    count++
+}
+let q2=prompt("What is the captial of france?")
+if(q2=="paris"){
+    count++
+}
+let q3=prompt("What is the colour of sky?")
+if(q3=="blue"){
+    count++;
+}
+console.log("Your score is:"+count)
