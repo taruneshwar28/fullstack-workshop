82bde749a1a2cc26d4de4279e4964af420c9f440
Narava Tarun Eshwar <142734331+taruneshwar28@users.noreply.github.com>
2025-12-28 20:42:53 +0530

    Complete 06-javascript exercise: mad-libs

---
+++ 06-javascript/mad-libs.js
@@ -0,0 +1,6 @@
+let user_name=prompt("Enter your name")
+let adjective=prompt("Enter a adjective")
+let noun=prompt("Enter a noun")
+let verb=prompt("Enter a verb")
+let place=prompt("Enter a place")
+console.log(`One day,${user_name} feels very ${adjective},so he picked up his ${noun} and started ${verb} towards the ${place}`);
