ed723f8113e1470cb6edd16734c7f7a864843678
Narava Tarun Eshwar <142734331+taruneshwar28@users.noreply.github.com>
2025-12-30 13:00:05 +0530

    Complete 06-javascript/challenge-13: Core JS challenge

--- 06-javascript/challenge-08/validate-password.js
+++ 06-javascript/challenge-08/validate-password.js
@@ -1,52 +1,59 @@
-function validatePassword(password) {
-    let isValid=false;
-    let up=0;
-    let lo=0;
-    let num=0;
-    let sp=0;
-    let error=[];
-    let suggestions=[];   
-    if(password.length<8){
-        error.push('Too short');
-    }
-    for(i of password){
-        if(i==i.toUpperCase()){
-            up++;
-        }
-        if(i==i.toLowerCase()){
-            lo++;
-        }
-        if (/\d/.test(i)){
-            num++;
-        }
-        else{
-            sp++;
-        }
-    }
-    if(up==0){
-        error.push('Need at least one uppercase');
-        suggestions.push('Add uppercases');
-        }
-    if(lo==0){
-       error.push('Need at least one lowercase');
-        suggestions.push('Add lowercases')
-    }
-    if(num==0){
-        error.push('Need at least one number')
-        suggestions.push('Add numbers');
-    }
-    if(sp==0){
-        suggestions.push('Add special character');
-        error.push('Add at least one special character')
-    }
-    if(error.length==0){
-        isValid=true;
-    }
-        let obj={valid:isValid,errors:error,suggestion:suggestions}
-        return obj;
+const validatePassword = (password) => {
+    let isValid = false;
 
-}
+    let up = 0;
+    let lo = 0;
+    let num = 0;
+    let sp = 0;
 
-console.log(validatePassword('abc'));
+    const errors = [];
+    const suggestions = [];
 
-console.log(validatePassword('MyP@ssw0rd!2024'));
+    if (password.length < 8) {
+        errors.push("Too short");
+        suggestions.push("Use at least 8 characters");
+    }
+
+    // array method used
+    [...password].map(char => {
+        if (char >= "A" && char <= "Z") up++;
+        else if (char >= "a" && char <= "z") lo++;
+        else if (/\d/.test(char)) num++;
+        else sp++;
+    });
+
+    // strict equality used everywhere
+    if (up === 0) {
+        errors.push("Need at least one uppercase letter");
+        suggestions.push("Add uppercase letters");
+    }
+
+    if (lo === 0) {
+        errors.push("Need at least one lowercase letter");
+        suggestions.push("Add lowercase letters");
+    }
+
+    if (num === 0) {
+        errors.push("Need at least one number");
+        suggestions.push("Add numbers");
+    }
+
+    if (sp === 0) {
+        errors.push("Need at least one special character");
+        suggestions.push("Add special characters");
+    }
+
+    if (errors.length === 0) {
+        isValid = true;
+    }
+
+    return {
+        valid: isValid,
+        errors,
+        suggestions,
+        message: `Password is ${isValid ? "valid" : "invalid"}`
+    };
+};
+
+console.log(validatePassword("abc"));
+console.log(validatePassword("MyP@ssw0rd!2024"));
