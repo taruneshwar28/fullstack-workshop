8920ec38b9261e2f181e1a95b696647ec18bffdf
Narava Tarun Eshwar <142734331+taruneshwar28@users.noreply.github.com>
2025-12-30 13:51:31 +0530

    Update number-pyramid.js

--- 06-javascript/number-pyramid.js
+++ 06-javascript/number-pyramid.js
@@ -1,16 +1,22 @@
-let n=5;
-let t="1";
-for(let i=0;i<n;i++){
-    let row="";
-    for(let j=0;j<n;j++){
-        if(j==n-i-1){
-            let x=parseInt(t)*parseInt(t)
-            row+=String(x)
-            t+="1"
-        }
-        else{
-            row+=" ";
-        }
-    }
-    console.log(row);
-}
+const n = 5;
+let t = "1";
+
+const generatePattern = () => {
+    Array.from({ length: n }).map((_, i) => {
+        let row = "";
+
+        Array.from({ length: n }).map((_, j) => {
+            if (j === n - i - 1) {
+                const x = Number(t) * Number(t);
+                row += `${x}`;
+                t += "1";
+            } else {
+                row += " ";
+            }
+        });
+
+        console.log(row);
+    });
+};
+
+generatePattern();
