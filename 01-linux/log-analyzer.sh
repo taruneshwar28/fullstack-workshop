8b20a4f862c5be06eb952e1c1d57c3071aa49d23
Narava Tarun Eshwar <142734331+taruneshwar28@users.noreply.github.com>
2025-12-30 12:48:52 +0530

    Complete 01-linux exercise: log analyzer

--- 01-linux/log-analyzer.sh
+++ 01-linux/log-analyzer.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
+# If user passes a file path as argument, use it
+# Otherwise, use default log file path
 LOG_FILE="/c/Users/Tarun/Desktop/revature/sample-log.txt"
 echo "===================LOG ANALYSIS REPORT================="
 echo "File: $LOG_FILE"
+# Check if file exists
 if [ ! -e $LOG_FILE ]; then
         echo "Error : Log file not found"
         exit 1
 else
+# Count total number of lines
         count=0
         while read line; do
                 ((count++))
