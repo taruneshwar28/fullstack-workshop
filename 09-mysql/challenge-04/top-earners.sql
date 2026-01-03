39111343ae3fc3574e103731d98cfa4588062030
Narava Tarun Eshwar <142734331+taruneshwar28@users.noreply.github.com>
2026-01-03 10:33:22 +0530

    Delete 09-mysql/challenge-04 directory

--- 09-mysql/challenge-04/top-earners.sql
+++
@@ -1,1 +0,0 @@
-select department,name,salary,rank_in_dept from (select department,name,salary,rank() over(partition by department order by salary desc) as rank_in_dept from employees) as e where rank_in_dept<=3;
